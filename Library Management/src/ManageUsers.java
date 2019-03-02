a6dad042fdf51210ab0cf353bc17c3feb5f1be80
Nadramon <35609987+Nadramon@users.noreply.github.com>
2019-03-02 10:34:33 -0700

    Can move between archive and active now

--- Library Management/src/ManageUsers.java
+++ Library Management/src/ManageUsers.java
@@ -2,22 +2,30 @@ import java.util.ArrayList;
 
 public class ManageUsers {
 
-	static ArrayList<Student> studentList = new ArrayList<Student>();
-	
+	static ArrayList<Student> activeList = new ArrayList<Student>();
+	static ArrayList<Student> archiveList = new ArrayList<Student>();
 	
 	
 	public static void main(String[] args) {
 
 		//make fake students
 		Student james = new Student("James", "Obama", 213, 23, true);
-		studentList.add(james);
+		activeList.add(james);
 		Student bob = new Student("Bob", "Bobb", 123, 5, true);
-		studentList.add(bob);
+		activeList.add(bob);
 		Student sally = new Student("Sally", "Swings", 435, 3, true);
-		studentList.add(sally);
+		activeList.add(sally);
 		
 		
-		ManageUsersGUI obama = new ManageUsersGUI(studentList);
+		
+		Student tina = new Student("Tina", "Tyna", 2311, 5, false);
+		archiveList.add(tina);
+		Student michael = new Student("Michael", "Mann", 111, 3, false);
+		archiveList.add(michael);
+		
+		
+		
+		ManageUsersGUI obama = new ManageUsersGUI(activeList, archiveList);
 		//obama.startGUI(studentList);
 		
 	}
--- Library Management/src/ManageUsersGUI.java
+++ Library Management/src/ManageUsersGUI.java
@@ -8,14 +8,37 @@ import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
+import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.SwingConstants;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.SpringLayout;
+import javax.swing.JSeparator;
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowEvent;
 
 public class ManageUsersGUI {
 
 	private JFrame frame;
 	private JTable table;
-	private ArrayList<Student> list = new ArrayList<Student>();
+	private ArrayList<Student> activeList = new ArrayList<Student>();
+	private ArrayList<Student> archiveList = new ArrayList<Student>();
+	private JLabel lblActiveDatabase;
+	private JButton btnNewButton;
+	private JLabel lblNewLabel;
+	private JTable table_1;
+	private JButton btnNewButton_1;
+	private JTable table_2;
+	private DefaultTableModel dataActive;
+	private DefaultTableModel dataArchive;
 
 	/**
 	 * Launch the application.
@@ -35,10 +58,12 @@ public class ManageUsersGUI {
 
 	/**
 	 * Create the application.
+	 * @wbp.parser.entryPoint
 	 */
-	public ManageUsersGUI(ArrayList<Student> lululu) {
+	public ManageUsersGUI(ArrayList<Student> active, ArrayList<Student> archive) {
 		
-		list = lululu;
+		activeList = active;
+		archiveList = archive;
 		
 		
 		
@@ -51,59 +76,192 @@ public class ManageUsersGUI {
 		}
 	}
 	
-	
-	
-	public void startGUI(ArrayList<Student> lululu) {
-		
-		list = lululu;
-		
-		try {
-			this.frame.setVisible(true);
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		//initialize();
-		
-		
-	}
-	
+
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 572, 420);
+		frame.setBounds(100, 100, 572, 603);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JButton btnNewButton = new JButton("Switch to Archive");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		frame.getContentPane().add(btnNewButton, BorderLayout.NORTH);
-		
-		table = new JTable();
-		DefaultTableModel dataModel = new DefaultTableModel(0, 0);
 		
 		
-		String header[] = new String[] {"First Name", "Last Name", "UCID", "Borrowing", "Archive"};
-		
-		dataModel.setColumnIdentifiers(header);
+		dataActive = new DefaultTableModel(0, 0);
 		
 		
-		table.setModel(dataModel);
+		String header[] = new String[] {"Number", "First Name", "Last Name", "UCID", "Borrowing"};
 		
-		dataModel.addRow(new Object[] {"F Name", "L Name", "UCID", "Borrowing", "Archive"});
+		dataActive.setColumnIdentifiers(header);
 		
-		for (int x = 0; x <= list.size(); x++) {
-	        dataModel.addRow(new Object[] {list.get(x).getFirstName(), list.get(x).getLastName(), list.get(x).getUcid(), list.get(x).getCurrentBorrowing(), "N" });
+		dataActive.addRow(new Object[] {"No.", "F Name", "L Name", "UCID", "Borrowing"});
+		
+		for (int x = 0; x <= activeList.size() - 1; x++) {
+			dataActive.addRow(new Object[] {x, activeList.get(x).getFirstName(), activeList.get(x).getLastName(), activeList.get(x).getUcid(), activeList.get(x).getCurrentBorrowing()});
 		}
 		
 		
-		frame.getContentPane().add(table, BorderLayout.CENTER);
+		frame.getContentPane().setLayout(new GridLayout(0, 1, 0, 0));
+		
+		lblActiveDatabase = new JLabel("Active Accounts");
+		lblActiveDatabase.setHorizontalAlignment(SwingConstants.CENTER);
+		frame.getContentPane().add(lblActiveDatabase);
+		
+		table = new JTable();
+		
+		
+		table.setModel(dataActive);
+		frame.getContentPane().add(table);
+		
+		btnNewButton = new JButton("Move to Archive");
+		
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				//activeList.clear();
+				//frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+				
+				//dataActive.removeRow(0);
+				//table.setModel(dataActive);
+				
+				
+				String idS = JOptionPane.showInputDialog(frame, "Which No. would you like to move?", null);
+				
+				for (int x=0; x < archiveList.size(); x++) {
+					dataArchive.removeRow(1);
+				}
+				
+				for (int x=0; x < activeList.size(); x++) {
+					dataActive.removeRow(1);
+				}
+				
+				
+				
+				int id = Integer.parseInt(idS);
+				
+				archiveList.add(activeList.get(id));
+				
+				activeList.remove(activeList.get(id));
+				
+				
+				for (int x = 0; x <= activeList.size() - 1; x++) {
+					dataActive.addRow(new Object[] {x, activeList.get(x).getFirstName(), activeList.get(x).getLastName(), activeList.get(x).getUcid(), activeList.get(x).getCurrentBorrowing()});
+				}
+				
+								
+				
+				
+				
+				
+				table.setModel(dataActive);
+				
+				for (int x = 0; x <= archiveList.size() - 1; x++) {
+					dataArchive.addRow(new Object[] {x, archiveList.get(x).getFirstName(), archiveList.get(x).getLastName(), archiveList.get(x).getUcid(), archiveList.get(x).getCurrentBorrowing()});
+				}
+				
+				
+				
+				table_1.setModel(dataArchive);
+				
+			}
+		});
+
+		
+		
+		
+		frame.getContentPane().add(btnNewButton);
+		
+		table_2 = new JTable();
+		frame.getContentPane().add(table_2);
+		
+		lblNewLabel = new JLabel("Archived Accounts");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		frame.getContentPane().add(lblNewLabel);
+		
+		
+		
+		dataArchive = new DefaultTableModel(0, 0);
+
+		
+		dataArchive.setColumnIdentifiers(header);
+		
+		dataArchive.addRow(new Object[] {"No.", "F Name", "L Name", "UCID", "Borrowing"});
+		
+		for (int x = 0; x <= archiveList.size() - 1; x++) {
+			dataArchive.addRow(new Object[] {x, archiveList.get(x).getFirstName(), archiveList.get(x).getLastName(), archiveList.get(x).getUcid(), archiveList.get(x).getCurrentBorrowing()});
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		table_1 = new JTable();
+		table_1.setModel(dataArchive);
+		frame.getContentPane().add(table_1);
+		
+		btnNewButton_1 = new JButton("Move to Active");
+		
+		
+		btnNewButton_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				//activeList.clear();
+				//frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+				
+				//dataActive.removeRow(0);
+				//table.setModel(dataActive);
+				
+				
+				String idS = JOptionPane.showInputDialog(frame, "Which No. would you like to move?", null);
+				
+				for (int x=0; x < archiveList.size(); x++) {
+					dataArchive.removeRow(1);
+				}
+				
+				for (int x=0; x < activeList.size(); x++) {
+					dataActive.removeRow(1);
+				}
+				
+				
+				
+				int id = Integer.parseInt(idS);
+				
+				activeList.add(archiveList.get(id));
+				
+				archiveList.remove(archiveList.get(id));
+				
+				for (int x = 0; x <= archiveList.size() - 1; x++) {
+					dataArchive.addRow(new Object[] {x, archiveList.get(x).getFirstName(), archiveList.get(x).getLastName(), archiveList.get(x).getUcid(), archiveList.get(x).getCurrentBorrowing()});
+				}
+				
+				
+				
+								
+				
+				table_1.setModel(dataArchive);
+				
+				
+				
+				
+				for (int x = 0; x <= activeList.size() - 1; x++) {
+					dataActive.addRow(new Object[] {x, activeList.get(x).getFirstName(), activeList.get(x).getLastName(), activeList.get(x).getUcid(), activeList.get(x).getCurrentBorrowing()});
+				}
+				
+				
+				table.setModel(dataActive);
+				
+				
+			}
+		});
+		
+		
+		
+		frame.getContentPane().add(btnNewButton_1);
 		
 	}
 	
